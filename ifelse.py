e0560057104b95b983c182520369469620481b23
Arti5070 <106901203+Arti5070@users.noreply.github.com>
2022-06-05 12:02:04 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,9 @@
+# name= int(input("enter the emplyes,s name"))
+# salery=int(input("enter the salery"))
+# year=int(input("enter the four servic"))
+# if year>=5:
+#     print("the empleys will get bounse of 5%")
+#     bounse=salery+salery*0.05
+#     print("so the bounse is,bonuse")
+# else:
+#     print("no bounes")                                      
